7b2c48a6544e80886382614d318de59663683bcf
Steve King <steve@kwsites.co.uk>
2011-12-28 08:17:25 +0100

    Move content to src directory to make space for a test directory. Include first jstestdriver test case Create cookie and cookiejar as start of cookie persistence support

---
+++ src/index.js
@@ -0,0 +1,28 @@
+var Url = require('url'),
+   http = require('http'),
+   https = require('https'),
+   Session = require('./session');
+
+/**
+ * The Xhr object is the external interface to the Session object (though Session can be used directly to
+ * allow for custom transport mechanisms), and requires a valid fully qualified URL and a configuration
+ * object that includes any options for the request such as the success/error/complete handlers and
+ * the method/data to send.
+ *
+ * @param {String} url
+ * @param {Object} config
+ * @return {Session}
+ */
+function Xhr(url, config) {
+   var parsedUrl = Url.parse(url),
+      transport = http;
+
+   if (parsedUrl.protocol == 'https:') {
+      transport = https;
+      parsedUrl.port = parsedUrl.port || 443;
+   }
+
+   return new Session(parsedUrl, config, transport);
+}
+
+module.exports = Xhr;
