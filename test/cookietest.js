7730985f47e563f3e68370a0ce557b2e9f172fec
Steve King <steve@kwsites.co.uk>
2011-12-28 08:31:32 +0100

    Test for expired cookie behaviour

--- test/cookietest.js
+++ test/cookietest.js
@@ -4,10 +4,23 @@ var module = {};
 TestCase("CookieTest", {
 
    'test reads valid cookie': function() {
-      var cookie = Cookie.build('PREF=ID=3bda468c998e6d4d:FF=0:TM=1324881665:LM=1324881665:S=B-PvMd5huxfc9WPP; expires=Wed, 25-Dec-2013 06:41:05 GMT; path=/; domain=.google.co.uk');
+      var expiryYear = (new Date).getFullYear() + 1,
+          expiryString = 'Wed, 25-Dec-' + expiryYear + ' 06:41:05 GMT',
+          valueString = 'ID=blah:FF=0:TM=2020202020:LM=1010101010:S=B-PcPxpPxPxxpP';
+
+      var cookie = Cookie.build('PREF=' + valueString + '; expires=' + expiryString + '; path=/; domain=.google.co.uk');
       assertTrue(cookie !== null);
-      assertEquals(+new Date('Wed, 25-Dec-2013 06:41:05 GMT'), +cookie.expires);
-      assertEquals('ID=3bda468c998e6d4d:FF=0:TM=1324881665:LM=1324881665:S=B-PvMd5huxfc9WPP', cookie.value);
+      assertEquals(+new Date(expiryString), +cookie.expires);
+      assertEquals(valueString, cookie.value);
+   },
+
+   'test build returns no cookie when it should have expired': function() {
+      var expiryYear = (new Date).getFullYear() - 1,
+         expiryString = 'Wed, 25-Dec-' + expiryYear + ' 06:41:05 GMT',
+         valueString = 'ID=blah:FF=0:TM=2020202020:LM=1010101010:S=B-PcPxpPxPxxpP';
+
+      var cookie = Cookie.build('PREF=' + valueString + '; expires=' + expiryString + '; path=/; domain=.google.co.uk');
+      assertEquals(null, cookie);
    }
 
 });
